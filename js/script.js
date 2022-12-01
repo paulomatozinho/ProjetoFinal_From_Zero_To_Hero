f6978857d7ef9aa955d27aabeb2a7a10fc7c82a2
paulomatozinho <paulo.matozinho@gmail.com>
2022-12-01 17:10:57 -0300

    tela de login finalizada

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ menu.addEventListener('click', () => {
 })
 
 // Validação Formulário
-const form   = document.getElementById('form');
+const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
-const span   = document.querySelectorAll('.span-required');
+const span = document.querySelectorAll('.span-required');
+
+function validacao() {
+    console.log('iniciando validacao');
+    let cpf = document.getElementById('cpf_digitado').value;
+    console.log(cpf.length);
+    if (cpf.length == 11) {
+        console.log('cpf ok')
+    } else {
+        console.log('cpf invalido')
+    }
+}

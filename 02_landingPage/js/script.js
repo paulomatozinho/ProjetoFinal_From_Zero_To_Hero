acf7623ab5f28cde0b4b4911931d75c2ec37d317
paulomatozinho <paulo.matozinho@gmail.com>
2022-12-11 09:02:58 -0300

    revisando estrutura da index.html

--- 02_landingPage/js/script.js
+++ 02_landingPage/js/script.js
@@ -8,7 +8,7 @@ menu.addEventListener('click', () => {
 })
 
 //Carrossel
-let trocaImagem = 5000;
+let trocaImagem = 4000;
 
 function banner1() {
     document.getElementById("banner").src = "img/img1.jpg";
@@ -27,33 +27,13 @@ function banner3() {
 
 
 // Validação do CPF
-
-
-let form = document.getElementById('form');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-
-let spanMensagem = document.getElementsByClassName('span-required');
-
-//validar quando usuário sai do campo
-name.onblur = function (event) {
-    if (event.target.value.length < 3) {
-        spanMensagem.textContent = "tudo ok";
-        spanMensagem.style.display = 'block';
+function validacao() {
+    console.log('iniciando validacao');
+    let cpf = document.getElementById('cpf_digitado').value;
+    console.log(cpf.length);
+    if (cpf.length == 11) {
+        alert('Suas informações foram enviadas')
+    } else {
+        alert('CPF inválido')
     }
 }
-
-
-
-
-
-// function validacao() {
-//     console.log('iniciando validacao');
-//     let cpf = document.getElementById('cpf_digitado').value;
-//     console.log(cpf.length);
-//     if (cpf.length == 11) {
-//         alert('Suas informações foram enviadas')
-//     } else {
-//         alert('cpf invalido')
-//     }
-// }
